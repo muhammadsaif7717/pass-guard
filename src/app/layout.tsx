f5f87d80a1e9c79f11fd52b79cd8a687ba66fe1b
MD. SAIF ISLAM <muhammadsaif7717@gmail.com>
2025-05-08 03:10:47 +0600

    Added password manager page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Navbar from "@/components/ui/Navbar";
 import { ThemeProvider } from "@/providers/theme-provider";
+import Navbar from "@/components/Navbar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +27,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>
+        <ClerkProvider
+
+        >
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import PasswordManager from "@/components/PasswordManager";
 import { useSession } from "@clerk/nextjs";
 
 export default function Home() {
   const data = useSession();
   console.log(data.session?.user);
-  return <div></div>;
+  return <div>
+    <PasswordManager />
+  </div>;
 }
